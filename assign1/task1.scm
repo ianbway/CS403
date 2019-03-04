616d177278b7f4abdf5627284f50458ef6917565
Ian Braudaway <iwbraudaway@crimson.ua.edu>
2019-03-03 21:35:34 -0600

    task1 looks finished. 8  more

--- assign1/task1.scm
+++ assign1/task1.scm
@@ -37,17 +37,17 @@
 	)
 
 (define (cyan value)
-	(int (+ (* 255 (cos (* (/ 3.14159265358979323846 200) value))) 0.00000001))
+	(int (+ (* 255.0 (cos (* (/ 3.14159265358979323846 200.0) value))) 0.00000001))
 	)
 
 
 (define (yellow value)
-	(int (+ (+ 255 (* 255 (sin (* (/ (* -2 3.14159265358979323846) 200) value)))) 0.00000001))
+	(int (+ (+ 255.0 (* 255.0 (sin (* (/ (* -2.0 3.14159265358979323846) 200.0) value)))) 0.00000001))
 	)
 
 
 (define (magenta value)
-	(int (+ (+ (/ 255 2) (* (/ 255 2) (cos (* (/ (* -3 3.14159265358979323846) 200) value)))) 0.00000001))
+	(int (+ (+ (/ 255.0 2.0) (* (/ 255.0 2.0) (cos (* (/ (* -3.0 3.14159265358979323846) 200.0) value)))) 0.00000001))
 	)
 
 (define (cym value)
@@ -60,10 +60,10 @@
 (define (main)
 	(setPort (open (getElement ScamArgs 1) 'read))
 	(define arg (readExpr))
-	(inspect (cyan arg))
-	(inspect (yellow arg))
-	(inspect (magenta arg))
-	(inspect (cym arg))
+	(println(string+ "(cyan " arg ") is " (cyan arg)))
+	(println(string+ "(yellow " arg ") is " (yellow arg)))
+	(println(string+ "(magenta " arg ") is " (magenta arg)))
+	(println(string+ "(cym " arg ") is " (cym arg)))
 	)
 
 (inspect (cym 5))
@@ -85,4 +85,5 @@
 (inspect (cym 85))
 (inspect (cym 90))
 (inspect (cym 95))
+(inspect (cym 98.23))
 (inspect (cym 100))
