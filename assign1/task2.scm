ea58bcfa1526fb8fd079d8350edaa9064cbba394
Ian Braudaway <iwbraudaway@crimson.ua.edu>
2019-03-17 12:58:44 -0500

    working on task 2 + 3

--- assign1/task2.scm
+++ assign1/task2.scm
@@ -1,13 +1,32 @@
 ; Ian Braudaway
 ; CS403 Assign 1, Task 2
 
-(define (mandelbrot value)
+(define (rCalc r s x)
 
+	(+ (- (* r r) (* s s)) x)
+
+	)
+
+(define (sCalc r s y)
+
+	(+ (* 2 (* r s)) y)
 	
+	)
+
+(define (threshold x y)
+
+	(cond (> (+ (* r r) (* s s)) 4) )
+
+	)
+
+(define (mandelbrot value)
+	(cond (> (threshold x y) value) 0 )
 	)
 
 (define (main)
 	(setPort (open (getElement ScamArgs 1) 'read))
-	(define arg (readExpr))
-	(println (mandelbrot arg))
+	(define argOne (readExpr))
+	(define argTwo (readExpr))
+	(define argThree (readExpr))
+	(println ((mandelbrot argOne) argTwo argThree))
 	)
--- assign1/task3.scm
+++ assign1/task3.scm
@@ -1,30 +1,41 @@
 ; Ian Braudaway
 ; CS403 Assign 1, Task 3
 
-(define (main)
-	(setPort (open (getElement ScamArgs 1) 'read))
-	;(define arg (readExpr))
-	(inspect (root-n readInt readInt))
-)
-
 (define (square x)
 	(* x x)
-)
-
-(define (close-enough? guess x)
-	(< (abs (- (square guess) x)) 0.0000000000000001)
-)
-
-(define (root-n nRoot rootOf)
-
-	(cond (== )
-
-
-	(cond 
-		((close-enough? ) 
-			)
-		(else
-			(root-n ))
-
 	)
-)
+
+(define (good-enough? guess x)
+	(< (abs (- (square guess) x)) 0.0000000000000001)
+	)
+
+(define (average x y nthRoot) 
+	(/ (+ x y) nthRoot)
+	)
+
+(define (improve guess x nthRoot)
+	(average guess (/ x (^ guess (- nthRoot 1))) nthRoot)
+	)
+
+(define (root-iter guess x nthRoot)
+	(if (good-enough? guess x)
+		guess
+		(root-iter (improve guess x nthRoot)
+			x))
+	)
+
+(define (root-n nthRoot)
+	(define (root-iter 1.0 rootOf nthRoot)
+		(if (good-enough? guess x)
+		guess
+		(root-iter (improve guess x nthRoot)
+			x))
+		)
+	)
+
+(define (main)
+	(setPort (open (getElement ScamArgs 1) 'read))
+	(define argOne (readExpr))
+	(define argTwo (readExpr))
+	(println "((root-n " argOne ")" argTwo ") is " (fmt "%.15f" ((root-n argOne) argTwo)))
+	)
