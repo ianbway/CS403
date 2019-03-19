c43356d2c47d83e9fc9df25f153399316ce1d0c9
Ian Braudaway <iwbraudaway@crimson.ua.edu>
2019-03-19 11:45:33 -0500

    task2 close

--- assign1/task2.scm
+++ assign1/task2.scm
@@ -1,32 +1,24 @@
 ; Ian Braudaway
 ; CS403 Assign 1, Task 2
 
-(define (rCalc r s x)
-
-	(+ (- (* r r) (* s s)) x)
-
+(define (iter r s x y i)
+		(cond 
+			((> (+ (* r r) (* s s)) 4.0) i)
+			((== 100.0 i) 0.0)
+		(else (iter (+ (- (* r r) (* s s)) x) (+ (* 2.0 (* r s)) y) x y i+1.0))
+		)
 	)
 
-(define (sCalc r s y)
-
-	(+ (* 2 (* r s)) y)
-	
-	)
-
-(define (threshold x y)
-
-	(cond (> (+ (* r r) (* s s)) 4) )
-
-	)
-
-(define (mandelbrot value)
-	(cond (> (threshold x y) value) 0 )
+(define (mandelbrot t)
+	(define (resistance x y)
+		(iter 0.0 0.0 x y 0.0)
+		)
 	)
 
 (define (main)
 	(setPort (open (getElement ScamArgs 1) 'read))
-	(define argOne (readExpr))
-	(define argTwo (readExpr))
-	(define argThree (readExpr))
-	(println ((mandelbrot argOne) argTwo argThree))
+	(define threshold (readExpr))
+	(define x (readExpr))
+	(define y (readExpr))
+	(println ((mandelbrot threshold) x y))
 	)
