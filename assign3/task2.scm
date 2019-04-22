cf272b89bd7150445726e63d1a80d79509fffdd5
Ian Braudaway <iwbraudaway@crimson.ua.edu>
2019-04-22 18:00:07 -0500

    task2 progress

--- assign3/task2.scm
+++ assign3/task2.scm
@@ -1,11 +1,50 @@
 ; Ian Braudaway
 ; CS403 Assign 3, Task 2
 
-(define (replace symbol value)
-	(inspect symbol)
-	(inspect value)
+(define (checkThisPair functionToLookIn replaceThis withThis)
+	(define body (get 'code functionToLookIn))
 
 
+	(inspect body)
+	(inspect (car body))
+	(inspect (cadr body))
+	; (cond
+
+	; 	)
+
+
+	)
+
+(define (replace functionToLookIn listOfReplacements)
+	;(inspect functionToLookIn)
+	;(inspect (car functionToLookIn))
+	;(inspect (cadr functionToLookIn))
+	(define name (get 'name functionToLookIn))
+	;(inspect name)
+	;(inspect listOfReplacements)
+	(define params (get 'parameters functionToLookIn))
+	(define body (get 'code functionToLookIn))
+
+	;(set 'code listOfReplacements name)
+	;(inspect params)
+	;(inspect (car params))
+	; (inspect body)
+	; (inspect (car body))
+	; (inspect (cdr body))
+	; (inspect (cadr body))
+
+	; (inspect listOfReplacements)
+	; (inspect (car listOfReplacements))
+	; (inspect (cadr listOfReplacements))
+	(println "test")
+
+	(cond 
+		((nil? listOfReplacements) functionToLookIn)
+		(else
+			(checkThisPair functionToLookIn (car listOfReplacements) (cadr listOfReplacements))
+			(replace functionToLookIn (cddr listOfReplacements))
+			)
+		)
 	)
 
 (define (main)
